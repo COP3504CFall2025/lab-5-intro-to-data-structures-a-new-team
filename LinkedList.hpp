55480d442625169a2b430f22a6ceebee8d1aa349
Monroy-B <bryanmonroyaa@gmail.com>
2025-11-07 16:43:29 -0500

    Define functions for LinkedList

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -6,43 +6,169 @@ using namespace std;
 
 template <typename T>
 class LinkedList {
+  struct Node {
+    T data;
+    Node* prev;
+    Node* next;
+  };
+
+  Node* head;
+  Node* tail;
+  unsigned int count;
+
+	static void PrintForwardRecr(Node* node) {
+    if(!node) { return; }
+
+    std::cout << ' '  << node->data;
+    PrintForwardRecr(node->next);
+  }
+
+
+  static void PrintReverseRecr(Node* node) {
+    if(!node) { return; }
+
+    std::cout << ' '  << node->data;
+    PrintForwardRecr(node->prev);
+  }
+
 public:
 	// Behaviors
-	void printForward() const;
-	void printReverse() const;
+	void printForward() const {
+		if(!head) { return; }
+		std::cout << ' '  << head->data;
+
+    PrintForwardRecr(head->next);
+    std::cout << std::endl;
+	}
+	
+	void printReverse() const {
+		if(!tail) { return; }
+    std::cout << ' '  << tail->data;
+
+    PrintForwardRecr(tail->prev);
+    std::cout << std::endl;
+	}
 
 	// Accessors
-	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+	[[nodiscard]] unsigned int getCount() const { return count; }
+	Node* getHead() { return head; }
+	const Node* getHead() const { return head; }
+	Node* getTail() { return tail; }
+	const Node* getTail() const {return tail; }
 
 	// Insertion
-	void addHead(const T& data);
-	void addTail(const T& data);
+	void addHead(const T& data) {
+    // First, construct a node using data
+    Node* head = new Node{
+      data,
+      nullptr,
+      this->head
+    };
+
+    // Then, insert the node where it belongs.
+    if (!this->head) {
+      this->head = this->tail = head;
+    }
+    else {
+      this->head->prev = head;
+      this->head = head;
+    }
+	}
+	
+	void addTail(const T& data) {
+    // First, construct a node using data
+    Node* tail = new Node{
+      data,
+      this->tail,
+      nullptr
+    };
+
+    if (!this->tail) {
+      this->tail = this->head = tail;
+    }
+    else {
+      this->tail->next = tail;
+      this->tail = tail;
+    }
+	}
 
 	// Removal
-	bool removeHead();
-	bool removeTail();
-	void Clear();
+	bool removeHead() {
+		if (!this->head) { return false; }
 
-	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+    if (this->head == this->tail) {
+      delete this->head;
+      this->head = this->tail = nullptr;
+      return true;
+    }
+  
+    Node* next = this->head->next;
+    delete this->head;
+    if (next) { next->prev = nullptr; }
+    this->head = next;
 
-	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+    return true;
+  }
+  
+	bool removeTail() {
+		if (!this->tail) { return false; }
+
+    if (this->head == this->tail) {
+      delete this->head;
+      this->head = this->tail = nullptr;
+      return true;
+    }
+    
+    Node* prev = this->tail->prev;
+    delete this->tail;
+    if (prev) { prev->next = nullptr; }
+    this->tail = prev;
+
+    return true;
+  }
+  
+	void Clear() {
+		if(!head) { return; }
+
+    removeHead();
+    Clear();
+  }
 
 private:
-	// Stores pointers to first and last nodes and count
-	Node* head;
-	Node* tail;
-	unsigned int count;
+  void CopyNodeChain(Node* head) {
+    if (!head) { return; }
+    AddTail(head->data);
+    CopyNodeChain(head->next);
+  }
 
+public:
+	LinkedList<T>& operator=(const LinkedList<T>& rhs) {
+    Clear();
+    CopyNodeChain(rhs.head);
+    count = rhs.count;
+	}
+	
+	// Construction/Destruction
+	LinkedList() : head(nullptr), tail(nullptr), count(0) {}
+	LinkedList(const LinkedList<T>& list) {
+		CopyNodeChain(list.head);
+    count = list.count;
+	}
+
+	LinkedList(LinkedList<T>&& other) noexcept {
+		head = other.head;
+    tail = other.tail;
+    count = other.count;
+
+    other.head = nullptr;
+    other.tail = nullptr;
+    other.count = 0;
+	}
+	
+	~LinkedList() {
+		Clear();
+    tail = nullptr;
+	}
 };
 
 
