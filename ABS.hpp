40f942afa91f71fade9215356692e36854a2cf27
Monroy-B <bryanmonroyaa@gmail.com>
2025-11-13 03:15:33 -0500

    Add ABS implementation

--- ABS.hpp
+++ ABS.hpp
@@ -1,43 +1,152 @@
 #pragma once
 
 #include <cstddef>
-#include <stdexcept>
 #include "Interfaces.hpp"
+#include <cstring>
+#include <iostream>
 
 // Technically bad, but size_t isn't likely to conflict with any client code.
 using std::size_t;
 
 template<typename T>
 class ABS : public StackInterface<T> {
+private:
+    T* grow() {
+        capacity *= scale_factor;
+        T* new_array = new T[capacity];
+        std::memcpy(new_array, array, size);
+        return new_array;
+    }
+
+    T* shrink() {
+        capacity /= scale_factor;
+        T* new_array = new T[capacity];
+        std::memcpy(new_array, array, size);
+        return new_array;
+    }
+    
 public:
     // Big 5 + Parameterized Constructor
-    ABS();
-    explicit ABS(const size_t capacity);
-    ABS(const ABS& other);
-    ABS& operator=(const ABS& rhs);
-    ABS(ABS&& other) noexcept;
-    ABS& operator=(ABS&& rhs) noexcept;
-    ~ABS() noexcept override;
+    explicit ABS(const size_t capacity) {
+        this->array = new T[capacity];
+        this->capacity = capacity;
+        this->size = 0;
+    }
+    
+    ABS() : ABS(1) {}
+    
+    ABS(const ABS& other) : ABS(other.capacity) {
+        std::memcpy(array, other.array, other.size);
+        size = other.size;
+    }
+    
+    ABS& operator=(const ABS& rhs) {
+        capacity = rhs.capacity;
+        std::memcpy(array, rhs.array, rhs.size);
+        size = rhs.size;
+    }
+    
+    ABS(ABS&& other) noexcept {
+        array = other.array;
+        capacity = other.capacity;
+        size = other.capacity;
+
+        other.array = nullptr;
+        other.capacity = 0;
+        other.size = 0;
+    }
+    
+    ABS& operator=(ABS&& rhs) noexcept {
+        array = rhs.array;
+        capacity = rhs.capacity;
+        size = rhs.capacity;
+
+        rhs.array = nullptr;
+        rhs.capacity = 0;
+        rhs.size = 0;
+    }
+    
+    ~ABS() noexcept override {
+        delete[] array;
+        capacity = size = 0;
+    }
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override;
+    [[nodiscard]] size_t getSize() const noexcept override {
+        return size;
+    }
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
+    [[nodiscard]] size_t getMaxCapacity() const noexcept {
+        return capacity;
+    }
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] T* getData() const noexcept {
+        return array;
+    }
 
     // Push item onto the stack
-    void push(const T& data) override;
+    void push(const T& data) override {
+        if (size >= capacity) {
+            T* new_array = grow();
+            delete[] array;
+            array = new_array;
+        }
 
-    T peek() const override;
+        array[size] = data;
+        size += 1;
+    }
 
-    T pop() override;
+    T peek() const override {
+        return array[size - 1];
+    }
+
+    T pop() override {
+        size -= 1;
+        T& output = array[size];
+
+        if (capacity >= 4 && 4 * size <= capacity) {
+            T* new_array = shrink();
+            delete[] array;
+            array = new_array;
+        }
+        
+        return output;
+    }
+private:
+    void PrintHead(T* head, size_t size) {
+        if (size == 0) { return; }
+
+        std::cout << ' ' << head[0];
+        PrintTail(head + 1, size - 1);
+    }
+
+    void PrintTail(T* tail, size_t size) {
+        if (size == 0) { return; }
+
+        std::cout << ' ' << tail[0];
+        PrintTail(tail - 1, size - 1);
+    }
+
+public:
+    void PrintForward() {
+        // Print Head
+        std::cout << array[0];
+
+        // Print Tail
+        PrintTail(array + 1, size - 1);
+    }
+
+    void PrintReverse() {
+        std::cout << array[size - 1];
+
+        PrintHead(array + (size - 1), size - 1);
+    }
 
 private:
-    size_t capacity_;
-    size_t curr_size_;
-    T* array_;
-    static constexpr size_t scale_factor_ = 2;
+    size_t capacity;
+    size_t size;
+    T* array;
+    static constexpr size_t scale_factor = 2;
 };
